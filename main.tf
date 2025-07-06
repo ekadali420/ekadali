2db4000e4f192541e2bd760be27b1cc67c1a2dc7
ekadali <shahanakhatun317@gmail.com>
2025-07-06 08:52:22 +0530

    stg

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@ resource "azurerm_resource_group" "rg1" {
   name     = "${var.rg-name}${count.index}"
   location = var.rg-location
 }
+
+resource "azurerm_storage_account" "sa1" {
+  count = 3
+  name = "${var.rg-name}${count.index}sa"
+  resource_group_name = azurerm_resource_group.rg1[count.index].name
+  location = azurerm_resource_group.rg1[count.index].location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
